c9535469cadee7d5abb3062f424581e1806014e5
Andrew Haine <andrew@andrewhaine.co.uk>
2022-12-07 15:36:55 +0000

    🎅 [2022] Day 4 Solutions

---
+++ 2022/04/b.js
@@ -0,0 +1,26 @@
+/**
+ * Advent of code solution
+ * @Andrew_Haine
+ */
+
+import fs from 'fs';
+import { showAns, dump } from "../../utils/index.js";
+
+const input = fs.readFileSync(new URL('input.txt', import.meta.url), "utf8");
+
+const assignmentPairs = input.split("\n");
+
+const getSections = assignment => {
+  const [start, end] = assignment.split('-').map(Number);
+  return Array.from({length: (end - start) + 1}, (i, j) => start + j);
+}
+
+const fullyOverlappingAssignments = assignmentPairs.filter(assignmentPair => {
+  const [a, b] = assignmentPair.split(',');
+  const aSections = getSections(a);
+  const bSections = getSections(b);
+
+  return aSections.some(section => bSections.includes(section)) || bSections.some(section => aSections.includes(section));
+})
+
+showAns(fullyOverlappingAssignments.length)
